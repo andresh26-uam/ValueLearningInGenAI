8b1eb77f15098708b70aa5ffa8a10400871c4c44
Andrés Holgado (Damo) <andres.holgado@urjc.es>
2026-05-17 17:22:09 +0200

    Experiments

--- run_seeds.sh
+++ run_seeds.sh
@@ -3,6 +3,7 @@ GPUS=L40S:1
 DATASET=ultra
 CONFIG=run_configs/llama_linear_firstgr_thenvs.json
 
+EVAL_EVERY_STEPS=""
 if [[ "$CONFIG" == "run_configs/llama_rlhf_linear.json" ]]; then
   NAME=LlamaBTRM
 elif [[ "$CONFIG" == "run_configs/llama_linear_firstgr_thenvs.json" ]]; then
@@ -23,9 +24,17 @@ fi
 if [[ "$DATASET" == "ultra" ]]; then
   DISCORDANCE_EPSILON=0.25
   NUM_TRAIN_EPOCHS=10
+  if [[ "$CONFIG" == "run_configs/llama_linear_firstgr_thenvs.json" ]]; then
+    NUM_TRAIN_EPOCHS=15
+    EVAL_EVERY_STEPS="--eval_every_steps=200"
+  fi
 elif [[ "$DATASET" == "pku" ]]; then
   DISCORDANCE_EPSILON=0.5
   NUM_TRAIN_EPOCHS=100
+  if [[ "$CONFIG" == "run_configs/llama_linear_firstgr_thenvs.json" ]]; then
+    NUM_TRAIN_EPOCHS=150
+    EVAL_EVERY_STEPS="--eval_every_steps=200"
+  fi
 else
   echo "Unknown dataset: $DATASET" >&2
   exit 1
@@ -35,7 +44,7 @@ echo "GPUs: $GPUS"
 echo "Training with config $CONFIG on dataset: $DATASET with discordance_epsilon: $DISCORDANCE_EPSILON and num_train_epochs: $NUM_TRAIN_EPOCHS"
 echo "Run name: ${GPUS}${NAME}"
 
-for seed in 42 43 44 45; do
-  bash sbatch_from_json.sh $CONFIG --dataset=$DATASET --run_name="${GPUS}${NAME}" --seed=$seed --num_train_epochs=$NUM_TRAIN_EPOCHS --discordance_epsilon=$DISCORDANCE_EPSILON --gpus=$GPUS
+for seed in 42; do
+  bash sbatch_from_json.sh $CONFIG --dataset=$DATASET --run_name="${GPUS}${NAME}" --seed=$seed --num_train_epochs=$NUM_TRAIN_EPOCHS $EVAL_EVERY_STEPS --discordance_epsilon=$DISCORDANCE_EPSILON --gpus=$GPUS
 done
 
